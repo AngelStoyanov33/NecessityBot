c5dc97268d55927b261d89d00692be011553faee
AngelStoyanov33 <aststoyanov@gmail.com>
2021-06-18 16:51:48 +0300

    Initial commit

---
+++ src/main/java/com/angelstoyanov/necessitybot/NecessityBotApplication.java
@@ -0,0 +1,13 @@
+package com.angelstoyanov.necessitybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NecessityBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NecessityBotApplication.class, args);
+	}
+
+}
